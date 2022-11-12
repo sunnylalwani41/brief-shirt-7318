93f449aa6e2a2ea85aeaa38697fb0b39a4dc3653
sunnylalwani41 <sunnylalwani41@gmail.com>
2022-11-12 17:04:27 +0530

    project

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,46 +1,52 @@
-let arr=["./img/1.jpg", "./img/2.jpg", "./img/3.jpg","./img/4.jpg", "./img/5.jpg", "./img/6.png", "./img/7.jpg", "./img/8.jpg", "./img/9.avif", "./img/10.avif", "./img/11.jpg", "./img/12.jpg"];
-let count=3;
-let image=document.querySelectorAll(".movingImage>img");
+let arr=["./img/1.jpg", "./img/2.jpg", "./img/3.jpg","./img/4.jpg", "./img/5.jpg", "./img/6.jpg", "./img/7.jpg", "./img/8.jpg", "./img/9.jpg", "./img/10.jpg"];
+let count=0;
+let image=document.querySelector(".movingImage>img");
 let id;
 movingImage()
 function movingImage(){
     id=setInterval(()=>{
-        if(count>=9){
-            count=4;
+        if(count>=10){
+            count=0;
         }
-        for(let i=3; i>=0; i--){
-            image[i].src=arr[count-i];
-        }
+        
+        image.src=arr[count];
         count++;
-        console.log(count);
+        // console.log(count);
     }, 2000);
 }
 let setTime;
 document.querySelector(".left").addEventListener("click", ()=>{
     clearTimeout(setTime);
     clearInterval(id);
-    if(count<=3){
-        count=11;
+    console.log(count);
+    if(count==0){
+        count=9;
     }
     else{
         count--;
     }
-    for(let i=3; i>=0; i--){
-        image[i].src=arr[count-i];
-    }
+    console.log(count);
+    image.src=arr[count];
     setTime=setTimeout(()=>{count++,movingImage()}, 4000);
 });
 document.querySelector(".right").addEventListener("click", ()=>{
     clearTimeout(setTime);
     clearInterval(id);
-    if(count>=11){
-        count=3;
+    console.log(count);
+    if(count>=9){
+        count=0;
     }
     else{
         count++;
     }
-    for(let i=3; i>=0; i--){
-        image[i].src=arr[count-i];
-    }
+    console.log(count);
+    image.src=arr[count];
     setTime=setTimeout(()=>{count++,movingImage()}, 4000);
+})
+let hei=document.querySelector("nav");
+let visibility=document.querySelector(".menu");
+let burger=document.querySelector(".burger");
+burger.addEventListener("click", ()=>{
+    hei.classList.toggle("h-nav");
+    visibility.classList.toggle("visibility")
 })
